b07515d92b6b838cf44b52735b6aaf18d1292322
N A <ninadambade@gmail.com>
2021-01-30 06:17:53 +0530

    initial commit

---
+++ page1/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def bio(request):
+    return render(request, 'biography.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+def publications(request):
+    return render(request, 'publications.html')
+def research(request):
+    return render(request, 'research.html')
+
+def students(request):
+    return render(request, 'students.html')
