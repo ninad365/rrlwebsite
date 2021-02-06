ec5dac9471c9322f9ab8d9cddc73c3caf78c56de
ninad365 <ninadambade@gmail.com>
2021-02-07 03:56:34 +0530

    Initial commit pages

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='anyName'),
+    path('', views.index, name='nHomepage'),
+    path('bio/',views.bio, name='nBio'),
+    path('publications/',views.publications, name='nPublications'),
+    path('research/',views.research, name='nResearch'),
+    path('students/',views.students, name='nStudents'),
+    path('courses/',views.courses, name='nCourses'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -3,4 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return(HttpResponse("Homepage"))
+    return(render(request,'home.html'))
+
+def bio(request):
+    return(render(request,'biography.html'))
+
+def mypage(request):
+    return(render(request,'biography.html'))
+
+def courses(request):
+    return(render(request,'courses.html'))
+
+def publications(request):
+    return(render(request,'publications.html'))
+
+def research(request):
+    return(render(request,'research.html'))
+
+def students(request):
+    return(render(request,'students.html'))
